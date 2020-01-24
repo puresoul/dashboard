dbafd67700706e4e1d6a13119ae98d19213076cb
Puresoul <pures0u1@email.cz>
2020-01-24 19:58:35 +0100

    add template mods/funcs

--- dashboard/controller/index/index.go
+++ dashboard/controller/index/index.go
@@ -1,6 +1,8 @@
 package index
 
 import (
+	"reflect"
+	"fmt"
 	"github.com/puresoul/dashboard/dashboard"
 	"github.com/puresoul/dashboard/lib/config/flight"
 	"net/http"
@@ -13,7 +15,23 @@ func Load() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	c := flight.Context(w, r)
-	v := c.View.New("home/index.ace")
-	v.Vars["var"] = "Ahoj svete"
+	v := c.View.New("home/index")
+
+	s := reflect.ValueOf(&c).Elem()
+	typeOfT := s.Type()
+
+	out := "<pre>"
+
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		z := reflect.ValueOf(&f).Elem()
+		out = out+fmt.Sprint(typeOfT.Field(i).Name)+" -\n"
+		for y := 0; y < z.NumField(); y++ {
+			out = out+"       + - "+fmt.Sprint(z)+" - "+fmt.Sprint(z.Field(y))+"\n"
+		}
+		out = out+"              + -"+fmt.Sprint(z.Interface())+"\n"
+    }
+
+	v.Vars["var"] = out+"</pre>"
 	v.Render(w, r)
 }
--- lib/system/boot/boot.go
+++ lib/system/boot/boot.go
@@ -5,21 +5,32 @@ import (
 	"github.com/puresoul/dashboard/dashboard/controller"
 	"github.com/puresoul/dashboard/lib/config/env"
 	"github.com/puresoul/dashboard/lib/config/flight"
+	"github.com/puresoul/dashboard/lib/viewfunc/authlevel"
+	"github.com/puresoul/dashboard/lib/viewfunc/noescape"
 	"log"
 )
 
 func RegisterDashboard(config *env.Info) {
 	err := config.Session.SetupConfig()
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	mysqlDB, _ := config.MySQL.Connect(true)
+
 	controller.LoadRoutes()
-	// Store the variables in flight
 
 	config.View.SetTemplates(config.View.Root, config.View.Children)
-	flight.StoreConfig(*config)
 
-	// Store the database connection in flight
+	config.View.SetFuncMaps(
+		noescape.Map(),
+	)
+
+	config.View.SetModifiers(
+		authlevel.Modify,
+	)
+
+	flight.StoreConfig(*config)
 	flight.StoreDB(mysqlDB)
 }
